53b6d623118106f378a444cec90419395f34e547
vikramthete8379 <vikramthete8379@gmail.com>
2026-05-27 09:03:57 +0530

    Add files via upload

---
+++ Script1.sh
@@ -0,0 +1,6 @@
+echo "We are trying this practical from linux EC2"
+echo "We are from batch-33 and learning git/gihub"
+echo "We are learning Branching"
+echo "I am learning Git"
+echo "I am learning GitHub with batch-32"
+echo "I have started my career in DevOps"
